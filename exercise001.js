7f29c0f8850e5fcc74cacb1f8214030a3107e97c
judejay <judejay26@gmail.com>
2023-11-04 20:43:21 +0000

    Feature: Async await promise

--- exercise001.js
+++ exercise001.js
@@ -1,15 +1,24 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
-promise.then((res) => console.log(`Yay! Promise resolved with response ${res}`))
-.catch((error) => console.error(`Boo. Promise rejected with response: ${error} `));
+
+const getPromise = async () => {
+  try {
+    const result = await promise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getPromise();
