1eb945b2ddf2dd93508c4fcdb751fcebacb33c0d
judejay <judejay26@gmail.com>
2023-11-05 14:05:46 +0000

    fetch data with async await

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,13 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const fetchData = async (apiEndPoint) => {
+  try {
+   const data = await (await fetch(apiEndPoint)).json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }    
 };
 
 fetchData(jsonTypicode);
